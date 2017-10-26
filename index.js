d3a5fb5bda67213ca9dbfc659db1b741f383d428
sangheshkothari <33114610+sangheshkothari@users.noreply.github.com>
2017-10-26 18:24:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var code = 405;
 var server = http.createServer(function(request, response) {
     if (request.method == 'POST' && request.url == '/upload') {
         var voiceRequest = app.textRequest('hello');
-        console.log(request);
+        res.json({'x':'abxd'});
         voiceRequest.on('response', function(_response) {
             response.end(JSON.stringify(_response));
         });
